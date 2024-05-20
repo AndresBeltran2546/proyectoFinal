47f0ab950fc3b61ba01ad94537bb1815dacd835e
AndresBeltran2546 <131633934+AndresBeltran2546@users.noreply.github.com>
2024-05-20 15:50:38 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+  document.location.href =
+    "https://wa.me/+573046001002?text=Halo%20saya%20ingin%20bertanya%20tentang%20produk%20anda";
+});
